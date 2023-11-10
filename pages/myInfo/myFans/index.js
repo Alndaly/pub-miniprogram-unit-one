6835472ddcbee56e14f4f6e4626d0f6791a7fd41
Kinda Hall <1142704468@qq.com>
2023-11-10 22:52:35 +0800

    fix: 🐛 修复我的粉丝页面

--- pages/myInfo/myFans/index.js
+++ pages/myInfo/myFans/index.js
@@ -4,24 +4,24 @@ import { to } from "../../../utils/util";
 
 Page({
   data: {
-    page: 0,
-    fansList: [],
+    pageNum: 0,
+    fans: null,
     isLoading: false,
   },
 
   toUserInfo(e) {
-    const { id } = e.currentTarget.dataset.id;
+    const { id } = e.currentTarget.dataset;
     wx.$router.push("/pages/userInfo/index", { user_id: id });
   },
 
-  async onShow() {
+  async onLoad(options) {
     wx.showLoading({
       title: "加载中...",
     });
     const [res, err] = await to(userApi.getMyFans(0));
     if (err) {
       wx.showToast({
-        title: "出错啦",
+        title: err.data,
         icon: "error",
       });
       this.setData({
@@ -30,7 +30,7 @@ Page({
       return;
     }
     this.setData({
-      fansList: res.data.data,
+      fans: res.data,
       isLoading: false,
     });
     wx.hideLoading();
@@ -40,15 +40,10 @@ Page({
     wx.showLoading({
       title: "刷新中",
     });
-    const [res, err] = await to(
-      userApi.getMyFans(
-        0,
-        this.data.page === 0 ? 10 : (this.data.page + 1) * 10
-      )
-    );
+    const [res, err] = await to(userApi.getMyFans(0));
     if (err) {
       wx.showToast({
-        title: "出错啦",
+        title: err.data,
         icon: "error",
       });
       this.setData({
@@ -58,22 +53,23 @@ Page({
       return;
     }
     this.setData({
-      fansList: res.data.data,
+      fans: res.data,
       isLoading: false,
+      pageNum: 0,
     });
     wx.hideLoading();
     wx.stopPullDownRefresh();
   },
 
   async onReachBottom() {
-    // 显示加载中提高交互
+    const { pageNum, fans } = this.data;
     this.setData({
       isLoading: true,
     });
-    const [res, err] = await to(userApi.getMyFans(this.data.page + 1));
+    const [res, err] = await to(userApi.getMyFans(pageNum + 1));
     if (err) {
       wx.showToast({
-        title: "出错啦",
+        title: err.data,
         icon: "error",
       });
       this.setData({
@@ -81,10 +77,13 @@ Page({
       });
       return;
     }
-    let fansListNext = res.data.data;
+    const fansNext = res.data;
     this.setData({
-      page: fansListNext.list.length > 0 ? this.data.page + 1 : this.data.page,
-      "fansList.list": [...this.data.fansList.list, ...fansListNext.list],
+      page: fansNext.content.length > 0 ? pageNum + 1 : pageNum,
+      fans: {
+        ...fansNext,
+        content: [...fans.content, ...fansNext.content],
+      },
       isLoading: false,
     });
   },
