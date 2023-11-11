b18f15db14872c77d1a59a90d0de4e74651ffa5f
Kinda Hall <1142704468@qq.com>
2023-11-11 21:26:36 +0800

    fix: 🐛 修复我点赞的页面

--- pages/myInfo/myVote/index.js
+++ pages/myInfo/myVote/index.js
@@ -1,5 +1,5 @@
 // pages/myInfo/myPublish/index.js
-import ugcApi from "../../../api/post";
+import postApi from "../../../api/post";
 import { _ } from "../../../utils/underscore-min";
 import { to } from "../../../utils/util";
 
@@ -8,64 +8,20 @@ Page({
    * Page initial data
    */
   data: {
-    parent: {
-      id: -1,
-    },
-    pageVote: 0,
+    pageNum: 0,
     isLoading: false,
-    myVoteList: [],
-  },
-  // 关闭用户评论弹窗
-  onCloseUgcCommentAction(e) {
-    this.setData({
-      showUgcCommentAction: false,
-      ugcDetailOfCommentList: null,
-    });
-    setTimeout(() => {
-      if (typeof this.getTabBar === "function" && this.getTabBar()) {
-        this.getTabBar().setData({
-          show: true,
-        });
-      }
-    }, 300);
+    posts: null,
   },
 
-  onFocusCommentToCommentInput(e) {
-    this.setData({
-      parent: e.detail,
-      focus_input: true,
-    });
-  },
-
-  // 显示用户评论
-  async onShowUgcCommentAction(e) {
-    const [res, err] = await to(ugcApi.getUgcComment(e.detail.id));
-    if (err) {
-      wx.showToast({
-        title: "出错啦",
-        icon: "error",
-      });
-    }
-    this.setData({
-      showUgcCommentAction: true,
-      ugcCommentList: res.data.data,
-      ugcDetailOfCommentList: e.detail,
-    });
-    if (typeof this.getTabBar === "function" && this.getTabBar()) {
-      this.getTabBar().setData({
-        show: false,
-      });
-    }
-  },
-
-  async getNextVote() {
+  async getNextPage() {
+    const { pageNum, posts } = this.data;
     this.setData({
       isLoading: true,
     });
-    const [res, err] = await to(ugcApi.getMyVote(this.data.pageVote + 1));
+    const [res, err] = await to(postApi.getMyLiked("", pageNum + 1));
     if (err) {
       wx.showToast({
-        title: "出错啦",
+        title: err.data,
         icon: "error",
       });
       this.setData({
@@ -73,13 +29,13 @@ Page({
       });
       return;
     }
-    const voteListNext = res.data.data;
+    const postsNext = res.data;
     this.setData({
-      "myVoteList.list": [...this.data.myVoteList.list, ...voteListNext.list],
-      pageVote:
-        voteListNext.list.length > 0
-          ? this.data.pageVote + 1
-          : this.data.pageVote,
+      posts: {
+        ...postsNext,
+        content: [...posts.content, ...postsNext.content],
+      },
+      pageNm: postsNext.content.length > 0 ? pageNum + 1 : pageNum,
       isLoading: false,
     });
   },
@@ -91,44 +47,39 @@ Page({
     wx.showLoading({
       title: "加载中",
     });
-    const [res, err] = await to(ugcApi.getMyVote(0));
+    const [res, err] = await to(postApi.getMyLiked("", 0));
     if (err) {
       wx.showToast({
-        title: "出错啦",
+        title: err.data,
         icon: "error",
       });
       return;
     }
     this.setData({
-      myVoteList: res.data.data,
+      posts: res.data,
     });
     wx.hideLoading();
   },
 
   async onReachBottom(options) {
-    this.getNextVote();
+    this.getNextPage();
   },
 
   async onPullDownRefresh() {
     wx.showLoading({
       title: "刷新中...",
     });
-    const [res, err] = await to(
-      ugcApi.getMyVote(
-        0,
-        this.data.pageVote === 0 ? 10 : (this.data.pageVote + 1) * 10
-      )
-    );
+    const [res, err] = await to(postApi.getMyLiked("", 0));
     if (err) {
       wx.showToast({
-        title: "出错啦",
+        title: err.data,
         icon: "error",
       });
       wx.stopPullDownRefresh();
       return;
     }
     this.setData({
-      myVoteList: res.data.data,
+      posts: res.data,
     });
     wx.showToast({
       title: "刷新成功",
