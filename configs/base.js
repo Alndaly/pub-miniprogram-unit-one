d933d019a3bba599f363ad3b9b9faec12931af9e
Kinda Hall <1142704468@qq.com>
2023-12-15 16:21:30 +0800

    feat: 🎸 修改base配置文件

--- configs/base.js
+++ configs/base.js
@@ -1,17 +1,26 @@
+// tips: 策略模式是一种比较不错的设计模式，你可以自行在version对象中补充test/prod环境等
+
 const version = {
-  // 本地开发
+  // 本地开发 
   develop: {
-    UNIT_ONE_APP_ID: 6,
+
+    // UnitOne主服务
+    // API_URL: "http://127.0.0.1:8001",
     // SIGNATURE_URL: "http://127.0.0.1:8010",
+    UNIT_ONE_APP_ID: 6, // 改为你在UnitOne管理台中创建的应用的ID
+    API_URL: "https://api.unit-one.top", // 此处不要动，是UnitOne的后端服务
+
+    // 图片上传服务
     SIGNATURE_URL: "https://upload.unit-one.top",
-    API_URL: "http://127.0.0.1:8001",
-    // API_URL: "https://api.unit-one.top",
     OSS_BUCKET: "unit-one",
     OSS_REGION: "oss-cn-beijing",
-    // https://www.imgrender.cn 海报生成服务的apikey
-    // 海报生成也可以使用我搭建的服务（需要修改很多代码，官网文档比较详细，这里就不做范例了） 详情见官网 https://uniapi.top 但是因为服务器的关系 比较不稳定
+
+    // 海报生成服务
+    // tips: 默认海报生成服务用的是https://www.imgrender.cn，此处的key仅作为测试使用，有限额，如果遇到超额的情况请自行注册配置，也可以直接联系我。 
+    // 当然，海报生成也可以使用我自己搭建的服务，具体配置和开发详情见官网 https://uniapi.top/api-refrence/image.html#制作海报，但是因为个人服务器的关系，配置比较低，会比较卡啦～
     IMG_RENDER_API_KEY: "169914845695713274.1Rm1Nlo5frfNwTGHNfjK5eky2bfFLm1T",
     IMG_RENDER_URL: "https://api.imgrender.cn/open/v1/pics",
+
   },
 };
 
